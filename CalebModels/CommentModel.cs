161d0feb493c1db69b9051517f0b17c6329d0ecc
Caleb Sylvia <caleb_sylvia@yahoo.com>
2024-04-30 10:53:00 -0700

    Push for Main

--- CalebModels/CommentModel.cs
+++ CalebModels/CommentModel.cs
@@ -8,6 +8,7 @@ namespace task_tracker_group.CalebModels.DTO
     public class CommentModel
     {
         public int ID { get; set; }
+        public string Username { get; set; }
         public string Comment { get; set; }
         public DateTime DateMade { get; set; }
         public int UserID { get; set; }
