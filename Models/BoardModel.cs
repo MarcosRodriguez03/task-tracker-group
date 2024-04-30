364597c2a1af812cd1bbd5f17bf2a1fe2d37060b
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-04-30 11:46:41 -0700

    connected azure, created endpoints and models

---
+++ Models/BoardModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task_tracker_group.Models
+{
+    public class BoardModel
+    {
+      
+        // project ID
+        public int ID { get; set; }
+        
+        // user who created project
+        public int UserID { get; set; }
+
+        public string? ProjectName { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public UserModel()
+        {
+
+        }
+    }
+}
--- Services/BoardService.cs
+++ Services/BoardService.cs
@@ -7,6 +7,19 @@ namespace task_tracker_group.Services
 {
     public class BoardService
     {
-        
+        public bool CreateBoard(BoardModel newBoard)
+        {
+            newBoard.IsDeleted = false;
+            _context.Add(newBoard);
+            
+            RelationTableModel relationModel = new RelationTableModel();
+            
+            relationModel.UserID = newBoard.UserID;
+            relationModel.ProjectID = _context.ProjectInfo.Count() + 1;
+
+            _context.Add(relationModel);
+
+            return _context.SaveChanges() != 0;
+        }
     }
 }
